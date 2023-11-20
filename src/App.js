cd6a19070f7d74623fd975f5cc41be1840d4ddb5
Jean-Eudes Laguérie <Jean-Eudi.Lagurie>
2023-11-20 11:44:25 +0100

    Creation taches termines

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [tasksCompleted, setTasksCompleted] = useState([]);
   const [task, setTask] = useState('');
 
   const addTask = () => {
@@ -14,31 +15,45 @@ function App() {
 
   const deleteTask = (indexToDelete) => {
     setTasks(tasks.filter((_, index) => index !== indexToDelete)); // Supprime la tâche de la liste
+    setTasksCompleted([...tasksCompleted, tasks[indexToDelete]]);
+  };
+
+  const markTaskCompleted = (indexToMark) => {
+    setTasksCompleted([...tasksCompleted, tasks[indexToMark]]);
+    setTasks(tasks.filter((_, index) => index !== indexToMark));
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>SimpleTasker</h1>
-        <div>
-          <input
-            type="text"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            placeholder="Ajouter une nouvelle tâche..."
-          />
-          <button onClick={addTask}>Ajouter</button>
-        </div>
-        <ul>
-          {tasks.map((t, index) => (
-            <li key={index}>
-              {t}
-              <button onClick={() => deleteTask(index)}>Supprimer</button>
-            </li>
-          ))}
-        </ul>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>SimpleTasker</h1>
+          <div>
+            <input
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                placeholder="Ajouter une nouvelle tâche..."
+            />
+            <button onClick={addTask}>Ajouter</button>
+          </div>
+          <ul>
+            {tasks.map((t, index) => (
+                <li key={index}>
+                  {t}
+                  <button onClick={() => deleteTask(index)}>Supprimer</button>
+                </li>
+            ))}
+          </ul>
+          <ul>
+            {tasksCompleted.map((t, index) => (
+                <li key={index}>
+                  {t}
+                  <button onClick={() => markTaskCompleted(index)}>Tâche terminée</button>
+                </li>
+            ))}
+          </ul>
+        </header>
+      </div>
   );
 }
 
